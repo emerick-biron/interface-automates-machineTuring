1acf1b57179a9ec331e3da5cb80a5490fab88954
Emerick Biron <emerick.biron@etu-umontpellier.fr>
2021-10-05 09:03:01 +0200

    a

--- src/main/java/fr/umontpellier/iut/App.java
+++ src/main/java/fr/umontpellier/iut/App.java
@@ -1,20 +1,7 @@
 package fr.umontpellier.iut;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-
-import java.util.*;
-
-public class App extends Application {
-    private Stage primaryStage;
-
+public class App {
     public static void main(String[] args) {
-        launch(args);
-    }
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        this.primaryStage = stage;
-        primaryStage.setTitle("Automates");
     }
 }
