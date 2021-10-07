fbd7bf058778101a41b279a59a5b96daa4448f82
Biron Emerick <emerick.biron@gmail.com>
2021-10-07 13:54:17 +0200

    déplacement vueEtat

--- src/main/java/fr/umontpellier/iut/composants/VueEtat.java
+++ src/main/java/fr/umontpellier/iut/composants/VueEtat.java
@@ -4,6 +4,7 @@ import fr.umontpellier.iut.logique.Etat;
 import javafx.event.EventHandler;
 import javafx.geometry.Point2D;
 import javafx.scene.Cursor;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
@@ -25,6 +26,7 @@ public class VueEtat extends StackPane {
         this.etat = etat;
         cercle = new Circle(50, 50, 50, Color.RED);
         getChildren().add(cercle);
+        getChildren().add(new Label("10"));
         init();
     }
 
@@ -36,12 +38,24 @@ public class VueEtat extends StackPane {
         });
         setOnMouseReleased(mouseEvent -> setCursor(Cursor.HAND));
         setOnMouseDragged(mouseEvent -> {
+            //Permet de deplacer la vueEtat a la souris
             double deltaX = mouseEvent.getSceneX() - mouseX;
             double deltaY = mouseEvent.getSceneY() - mouseY;
-            relocate(getLayoutX() + deltaX, getLayoutY() + deltaY);
-            mouseX = mouseEvent.getSceneX();
-            mouseY = mouseEvent.getSceneY();
+            double newXpos = getLayoutX() + deltaX;
+            double newYPos = getLayoutY() + deltaY;
 
-        }); setOnMouseEntered(mouseEvent -> setCursor(Cursor.HAND));
+            //Permet de faire en sorte que la vue etat ne sorte pas de la vue automate
+            if (newXpos >= 0 && newXpos + getWidth() <= getParent().getBoundsInLocal().getMaxX()) {
+                setLayoutX(newXpos);
+                mouseX = mouseEvent.getSceneX();
+            }
+            //Permet de faire en sorte que la vue etat ne sorte pas de la vue automate
+            if (newYPos >= 0 && newYPos + getHeight() <= getParent().getBoundsInLocal().getMaxY()) {
+                setLayoutY(newYPos);
+                mouseY = mouseEvent.getSceneY();
+            }
+
+        });
+        setOnMouseEntered(mouseEvent -> setCursor(Cursor.HAND));
     }
 }
