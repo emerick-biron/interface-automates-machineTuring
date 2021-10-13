c1b00f2dcad8ad10521aec1a74f0ceb6f0fb407b
Emerick Biron <emerick.biron@etu-umontpellier.fr>
2021-10-13 16:22:25 +0200

    VueTransition

--- src/main/java/fr/umontpellier/iut/gui/VueTransition.java
+++ src/main/java/fr/umontpellier/iut/gui/VueTransition.java
@@ -3,8 +3,31 @@ package fr.umontpellier.iut.gui;
 import fr.umontpellier.iut.logique.Transition;
 import javafx.scene.Group;
 import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 
 public class VueTransition extends Group {
     private Transition transition;
     private VueAutomate vueAutomate;
+    private Line ligne;
+
+    public VueTransition(Transition transition, VueAutomate vueAutomate) {
+        this.transition = transition;
+        this.vueAutomate = vueAutomate;
+        ligne = new Line();
+
+        getChildren().add(ligne);
+
+        init();
+    }
+
+    public void init(){
+        Circle cercleEtatDep = vueAutomate.getVueEtat(transition.getEtatDepart()).getCercle();
+        Circle cercleEtatFin = vueAutomate.getVueEtat(transition.getEtatArrivee()).getCercle();
+
+        ligne.startXProperty().bind(cercleEtatDep.centerXProperty());
+        ligne.startYProperty().bind(cercleEtatDep.centerYProperty());
+        ligne.endXProperty().bind(cercleEtatFin.centerXProperty());
+        ligne.endYProperty().bind(cercleEtatFin.centerYProperty());
+    }
 }
