7f62c609a61d45f344ee3f20e6423ba5c0f284eb
Emerick Biron <emerick.biron@etu-umontpellier.fr>
2021-10-13 14:12:47 +0200

    VueTransition extend Group

--- src/main/java/fr/umontpellier/iut/gui/VueTransition.java
+++ src/main/java/fr/umontpellier/iut/gui/VueTransition.java
@@ -1,9 +1,10 @@
 package fr.umontpellier.iut.gui;
 
 import fr.umontpellier.iut.logique.Transition;
+import javafx.scene.Group;
 import javafx.scene.layout.StackPane;
 
-public class VueTransition extends StackPane {
+public class VueTransition extends Group {
     private Transition transition;
     private VueAutomate vueAutomate;
 }
