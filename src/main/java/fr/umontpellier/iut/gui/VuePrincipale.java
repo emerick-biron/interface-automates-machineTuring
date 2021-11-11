57da36860c919aac5587b317db2357d170b13e7c
emerick-biron <emerick.biron@gmail.com>
2021-11-11 18:54:21 +0100

    maj jar

--- src/main/java/fr/umontpellier/iut/gui/VuePrincipale.java
+++ src/main/java/fr/umontpellier/iut/gui/VuePrincipale.java
@@ -158,12 +158,13 @@ public class VuePrincipale extends BorderPane {
                 vueTransition.deSelectionner();
             }
 
-            StageSupTrans stageSupTrans =
-                    new StageSupTrans(vueAutomate.getVuesTransitionSelectionnes(), App.getPrimStage());
-            ArrayList<VueTransition> transitionsASupprimer = stageSupTrans.showOpenDialog();
-            for (VueTransition vueTransition : transitionsASupprimer) {
-                vueAutomate.getAutomate().supprimerTransition(vueTransition.getTransition());
-                vueTransition.deSelectionner();
+            if (vueAutomate.getVuesTransitionSelectionnes().size() > 0) {
+                StageSupTrans stageSupTrans = new StageSupTrans(vueAutomate.getVuesTransitionSelectionnes(), App.getPrimStage());
+                ArrayList<VueTransition> transitionsASupprimer = stageSupTrans.showOpenDialog();
+                for (VueTransition vueTransition : transitionsASupprimer) {
+                    vueAutomate.getAutomate().supprimerTransition(vueTransition.getTransition());
+                    vueTransition.deSelectionner();
+                }
             }
         }
     };
