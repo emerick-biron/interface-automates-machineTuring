2cbbbb7d16cd18c3b8909677d2af8c1eabf5abc1
Biron Emerick <emerick.biron@gmail.com>
2021-11-21 14:31:46 +0100

    refactor code

--- src/main/java/machines/logique/Transition.java
+++ src/main/java/machines/logique/Transition.java
@@ -2,7 +2,7 @@ package machines.logique;
 
 import machines.automates.logique.EtatAtmt;
 
-public class Transition<T extends Transition<T, E>, E extends Etat<E, T>> {
+public abstract class Transition<T extends Transition<T, E>, E extends Etat<E, T>> {
     private E etatDepart;
     private E etatArrivee;
     private char etiquette;
